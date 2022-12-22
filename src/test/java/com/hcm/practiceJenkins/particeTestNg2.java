dba51cb7f0e7585d3847a96a6e3e152b186beed9
ysreekanth01 <ysreekanth9191@gmail.com>
2022-12-22 10:29:16 +0530

    first commit

---
+++ src/test/java/com/hcm/practiceJenkins/particeTestNg2.java
@@ -0,0 +1,11 @@
+package com.hcm.practiceJenkins;
+
+import org.testng.annotations.Test;
+
+public class particeTestNg2 {
+	@Test
+	public void Test2() {
+		System.out.println("test2");
+	}
+
+}
--- src/test/java/com/hcm/practiceJenkins/practice1Test.java
+++ src/test/java/com/hcm/practiceJenkins/practice1Test.java
@@ -1,9 +1,10 @@
 package com.hcm.practiceJenkins;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class practice1Test {
-
+//@Parameters("sanity")
 	@Test
 	public void Sample() {
 		System.out.println("hi");
